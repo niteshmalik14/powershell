ba0791c93ec8970539844a22b48f67a386688b72
Nitesh Malik <55227425+niteshmalik14@users.noreply.github.com>
2022-05-05 00:24:36 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-new-item -path "D:\" -name helloish.txt -ItemType "file"
-add-content D:\helloish.txt -value "yolo"
-move-item -path "D:\helloish.txt" -destination "C:/" -Force
-get-content C:\helloish.txt
-clear-content C:\helloish.txt
+$a = test-path D:\
+if ($a -eq true)
+{
+echo "path exists"
+}
