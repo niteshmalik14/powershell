a3786fda96ad71839d68b8b218cbc84bd2061b2f
Nitesh Malik <55227425+niteshmalik14@users.noreply.github.com>
2022-05-05 00:16:21 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 new-item -path "D:\" -name helloish.txt -ItemType "file"
 add-content D:\helloish.txt -value "yolo"
 move-item -path "D:\helloish.txt" -destination "C:/" -Force
+get-content C:\helloish.txt
